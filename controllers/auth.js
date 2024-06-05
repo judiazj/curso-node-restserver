e0743786db08a2b2c407d74e87ac5295f3cbab9f
judiazj <judiazj@unal.edu.co>
2024-06-05 15:38:35 -0500

    Autenticacion con google signin

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcryptjs = require('bcryptjs')
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req, res = response) => {
@@ -49,6 +50,52 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSignIn = async(req, res) => {
+    const {id_token} = req.body;
+
+    try {
+        const {correo, nombre, img} = await googleVerify(id_token);
+        
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario){
+            // Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                rol: 'USER_ROLE',
+                google: true
+            }
+            usuario = new Usuario(data)
+            await usuario.save();
+        }
+
+        // Si el usuario en DB
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        // Generar JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
